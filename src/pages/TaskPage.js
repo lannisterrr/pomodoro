bae920b82801bdb424806ba8290f34c7b6991161
siddhant <siddhantp736@gmail.com>
2022-05-19 13:24:02 +0530

    [feat]: timer implemented

--- src/pages/TaskPage.js
+++ src/pages/TaskPage.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { usePomodoroContext } from '../context/state-context';
 
@@ -9,20 +10,79 @@ function TaskPage() {
     return tasks.find(task => task.id === taskId);
   }
   const task = getTaskDetails(state.tasks, taskId);
+  const timerTime = Number(task.userTime);
 
-  console.log(task);
+  const [startTimer, setStartTimer] = useState(false);
+  const [minutes, setMinutes] = useState(timerTime);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    if (startTimer) {
+      let myInterval = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
+        }
+        if (seconds === 0) {
+          if (minutes === 0) {
+            clearInterval(myInterval);
+          } else {
+            setMinutes(minutes - 1);
+            setSeconds(59);
+          }
+        }
+      }, 1000);
+      return () => {
+        clearInterval(myInterval);
+      };
+    }
+  });
+
+  const handleStartTimer = () => {
+    setStartTimer(true);
+  };
+
+  const handleReset = () => {
+    setMinutes(timerTime);
+    setSeconds(0);
+    setStartTimer(false);
+  };
 
   return (
     <div className="w-screen  min-h-screen bg-primary pt-8 ">
       <main className="mx-auto flex-1 bg-white max-w-[90%] md:max-w-[80%] min-h-[100vh] rounded-2xl grid grid-cols-2 ">
         <div className="p-4">
-          <h3>Timer</h3>
+          <h3>
+            Timer : {minutes}: {seconds}
+          </h3>
+          <button
+            className="bg-primary text-white font-bold uppercase text-sm px-3 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 mt-3 ease-linear transition-all duration-150"
+            type="button"
+            onClick={handleStartTimer}
+          >
+            Start Timer
+          </button>
+
+          <button
+            className="bg-secondary text-white font-bold uppercase text-sm px-3 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 mt-3 ease-linear transition-all duration-150"
+            type="button"
+            onClick={() => setStartTimer(false)}
+          >
+            Pause Timer
+          </button>
+
+          <button
+            className="bg-emerald-500 text-white font-bold uppercase text-sm px-3 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 mt-3 ease-linear transition-all duration-150"
+            type="button"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
         </div>
         <div className="p-2 flex flex-col">
           <h1 class="text-2xl md:text-5xl p-4 font-normal leading-normal mt-0 mb-2 text-secondary text-center mt-10">
             {task.title}
           </h1>
-          <h4 class="font-medium leading-tight text-2xl mt-0 mb-2 text-gray-700 text-center">
+          <h4 class="font-medium leading-tight text-xl mt-0 mb-2 text-gray-700 text-center">
             {task.description}
           </h4>
           <h4 class="font-medium leading-tight text-2xl mt-0 mb-2 text-gray-400 text-center">
